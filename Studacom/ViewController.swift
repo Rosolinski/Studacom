a31dca854d30cca0fa837807096a12e670d1dacf
Rosolinski <rosolinski@googlemail.com>
2017-03-22 18:14:21 +0000

    Initial Commit

---
+++ Studacom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Studacom
+//
+//  Created by Robin Osolinski on 22/03/2017.
+//  Copyright © 2017 Robin Osolinski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
